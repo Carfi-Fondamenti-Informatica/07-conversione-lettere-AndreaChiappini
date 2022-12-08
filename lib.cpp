2035b9367053370ae9e028c6723760dd82d4dc5f
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-08 16:39:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by Andrea Chiappini on 08/12/22.
+//
 
+#include "lib.h"
+
+bool control (char & x) {
+    if (x >= 97 && x <= 122) {
+        x=x-32;
+        return true;
+    } else if (x >= 65 && x <= 90) {
+        x=x+32;
+        return true;
+    }
+    return false;
+}
